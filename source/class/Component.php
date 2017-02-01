c23012f697b8e144eb65b1547734eb6a6c4820b2
Jean luc <you@example.com>
2017-02-01 13:23:19 +0100

    synchro

--- source/class/Component.php
+++ source/class/Component.php
@@ -3,97 +3,126 @@
 namespace PHPComponent;
 
 
-
-use Phi\Module\DOMTemplate\Traits\MustacheTemplate;
-use Phi\Traits\Collection;
+use PHPComponent\Traits\MustacheTemplate;
+use PHPComponent\Traits\Collection;
 
 class Component extends Template
 {
 
 
-    protected $attributeXPathQuery='//meta[@data-attribute-name]';
-    protected $attributeAttributeName='data-attribute-name';
+    protected $attributeXPathQuery = '//property[@name]';
+    protected $attributeAttributeName = 'name';
 
 
-
-
-    public function __construct($template=null) {
+    public function __construct($template = null)
+    {
         parent::__construct($template);
     }
 
-    public function loadFromDOMNode(\DOMElement $node) {
+    public function loadFromDOMNode(\DOMElement $node)
+    {
 
 
-        $this->dom=$this->createDomDocumentFromNode($node);
+        $this->dom = $this->createDomDocumentFromNode($node);
         $this->extractParametersFromDOM($this->dom);
         return $this;
     }
 
-    public function extractParametersFromDOM($dom) {
+    public function extractParametersFromDOM($dom)
+    {
 
-        $query=$this->attributeXPathQuery;
 
-        $xPath=new \DOMXPath($dom);
-        $nodes=$xPath->query($query);
+        if($dom->firstChild->attributes->length) {
+
+            foreach ($dom->firstChild->attributes as $attribute) {
+                $this->setVariable($attribute->name, $attribute->value);
+            }
+        }
+
+
+        $query = $this->attributeXPathQuery;
+
+        $xPath = new \DOMXPath($dom);
+        $nodes = $xPath->query($query);
+
 
         foreach ($nodes as $attributeNode) {
             /**
              * @var \DOMElement $attributeNode
              */
 
-            $attributeName=(string) $attributeNode->getAttribute($this->attributeAttributeName);
-            $this->setVariable($attributeName, $attributeNode->textContent);
+            $attributeName = (string)$attributeNode->getAttribute($this->attributeAttributeName);
+            //$this->setVariable($attributeName, $attributeNode->textContent);
+
+
+            $this->setVariable($attributeName, $dom->innerHTML($attributeNode));
+
         }
     }
 
 
-	public function bindAttributesValues($attributesValues) {
+    public function bindAttributesValues($attributesValues)
+    {
 
 
-		foreach ($this->getVariables() as $variableName =>  $value) {
+        foreach ($this->getVariables() as $variableName => $value) {
 
-			$buffer=$value
-			;
-			preg_replace_callback('`\{\{\{(.*?)\}\}\}`', function($matches) use ($variableName, $attributesValues) {
+            $buffer = $value;
+            preg_replace_callback('`\{\{\{(.*?)\}\}\}`', function ($matches) use ($variableName, $attributesValues) {
 
-				$variables=explode('.', $matches[1]);
+                $variables = explode('.', $matches[1]);
 
-				$currentValue=null;
+                $currentValue = null;
 
-				if(isset($attributesValues[$variables[0]])) {
+                if (isset($attributesValues[$variables[0]])) {
 
-					$currentValue=$attributesValues[$variables[0]];
-
-
-					array_shift($variables);
-
-					foreach ($variables as $subVariable) {
-						if(is_array($currentValue) && isset($currentValue[$subVariable])) {
-							$currentValue=$currentValue[$subVariable];
-						}
-						else if(is_object($currentValue) && isset($currentValue->$subVariable)) {
-							$currentValue=$currentValue->$subVariable;
-						}
-						else {
-							$currentValue=null;
-							break;
-						}
-					}
-				}
-				$this->setVariable($variableName, $currentValue);
-			}, $buffer);
-		}
-
-		return $this;
+                    $currentValue = $attributesValues[$variables[0]];
 
 
-	}
+                    array_shift($variables);
+
+                    foreach ($variables as $subVariable) {
+                        if (is_array($currentValue) && isset($currentValue[$subVariable])) {
+                            $currentValue = $currentValue[$subVariable];
+                        } else if (is_object($currentValue) && isset($currentValue->$subVariable)) {
+                            $currentValue = $currentValue->$subVariable;
+                        } else {
+                            $currentValue = null;
+                            break;
+                        }
+                    }
+                }
+                $this->setVariable($variableName, $currentValue);
+            }, $buffer);
+        }
+
+        return $this;
+    }
+
+    public function render($template = null, $values = null)
+    {
+
+        if ($template) {
+            $this->template = $template;
+        }
+
+
+        if (count($values)) {
+            $this->setVariables($values);
+        }
+
+
+        $output = $this->compileMustache($this->template, $this->getVariables());
+
+
+        $this->output = $output;
+        return $this->output;
+    }
 
 
 
-
-
-    public function __toString() {
+    public function __toString()
+    {
         return $this->getOutput();
     }
 
--- source/class/Template.php
+++ source/class/Template.php
@@ -3,9 +3,9 @@
 namespace PHPComponent;
 
 
-use Phi\DOMDocument;
+use PHPComponent\DOMDocument;
 use PHPComponent\Traits\MustacheTemplate;
-use Phi\Traits\Collection;
+use PHPComponent\Traits\Collection;
 
 class Template
 {
@@ -16,7 +16,7 @@ class Template
 
 
     protected $output;
-    protected  $template;
+    protected $template;
 
 
     protected $libXMLFlag;
@@ -24,53 +24,77 @@ class Template
     protected $rootNode;
 
 
-    protected $customTags=array();
+    protected $customTags = array();
 
-    protected $componentEnabled=false;
+    protected $componentEnabled = false;
 
-    protected $defaultComponentTagName='phi-component';
-    protected $componantClassNameAttributeName='data-instanceof';
+    protected $defaultComponentTagName = 'phi-component';
+    protected $componantClassNameAttributeName = 'data-instanceof';
 
-	protected $components=array();
-
-
+    protected $components = array();
 
 
-    public function __construct($template=null) {
-        $this->libXMLFlag=\LIBXML_HTML_NOIMPLIED | \LIBXML_HTML_NODEFDTD | \LIBXML_NOXMLDECL;
-        $this->template=$template;
+    public function __construct($template = null)
+    {
+        $this->libXMLFlag =
+            \LIBXML_HTML_NOIMPLIED
+            | \LIBXML_HTML_NODEFDTD
+            | \LIBXML_NOXMLDECL
+            | \LIBXML_NOENT
+            | \LIBXML_NOERROR
+            | \LIBXML_NOWARNING
+            | \LIBXML_ERR_NONE
+        ;
+
+        $this->template = $template;
     }
 
-    public function setTemplate($template) {
-        $this->template=$template;
+    public function setTemplate($template)
+    {
+        $this->template = $template;
         return $this;
     }
 
 
-    public function registerCustomTag($tagName, $callback) {
+    public function registerCustomTag($tagName, $callback)
+    {
 
-        $customTag=new CustomTag($tagName, $callback);
+        $customTagCallback = new CustomTag($tagName, $callback);
+        $this->customTags[$tagName] = $customTagCallback;
 
-        $this->customTags[$tagName]=$customTag;//$callback;
-
-
-        return $customTag;
-        return $this;
-    }
-
-    public function enableComponents($value=true) {
-        $this->componentEnabled=$value;
-        return $this;
+        return $customTagCallback;
     }
 
 
+    public function registerComponent($tagName, $componentName)
+    {
 
-    public function createDomDocumentFromNode(\DOMElement $node) {
+        $template = $this;
 
-        $valueNode=$node->cloneNode(true);
+        $this->registerCustomTag($tagName, function ($content, $node) use ($componentName, $template) {
+            $component = new $componentName;
+            $component->loadFromDOMNode($node);
+            $buffer = $component->render();
 
-        $valueDocument=new DOMDocument('1.0', 'utf-8');
-        $importedValueNode=$valueDocument->importNode($valueNode, true);
+            return $template->parseDOM($buffer);
+        });
+    }
+
+
+    public function enableComponents($value = true)
+    {
+        $this->componentEnabled = $value;
+        return $this;
+    }
+
+
+    public function createDomDocumentFromNode(\DOMElement $node)
+    {
+
+        $valueNode = $node->cloneNode(true);
+
+        $valueDocument = new DOMDocument('1.0', 'utf-8');
+        $importedValueNode = $valueDocument->importNode($valueNode, true);
         $valueDocument->appendChild($importedValueNode);
 
         return $valueDocument;
@@ -78,60 +102,64 @@ class Template
     }
 
 
+    public function parseDOM($buffer, $toHTML=false)
+    {
+
+        libxml_use_internal_errors(true);
+        $dom = new DOMDocument('1.0', 'utf-8');
+        $dom->loadHTML($buffer, $this->libXMLFlag);
+        libxml_clear_errors();
 
 
+        $this->rootNode = $dom->firstChild;
 
-    public function parseDOM($buffer) {
-
-        //libxml_use_internal_errors(true);
-
-        $this->dom=new DOMDocument('1.0', 'utf-8');
-
-        $this->dom->loadXML($buffer, $this->libXMLFlag);
-        $this->rootNode=$this->dom->firstChild;
-
-        if($this->componentEnabled) {
+        if ($this->componentEnabled) {
             $this->initializeComponentParsing();
         }
 
 
-        foreach ($this->customTags as $tagName=>$customTag) {
-            $query='//'.$tagName;
+        foreach ($this->customTags as $tagName => $customTag) {
+
+            $query = '//' . $tagName;
+
+            //$query = '//' . $tagName.'//*[not('.$tagName.')]';
+            //$query = '//*[not('.$tagName.')]'.'//' . $tagName;
+            //BBB//*[not(BBB)]
 
 
-            $xPath=new \DOMXPath($this->dom);
-            $nodes=$xPath->query($query);
-
-
+            $xPath = new \DOMXPath($dom);
+            $nodes = $xPath->query($query);
 
 
             foreach ($nodes as $node) {
-
-                $nodeContent=$this->dom->innerHTML($node);
-                $content=call_user_func_array(array($customTag, 'render'), array($nodeContent, $node));
-                $this->dom->replaceNodeWithContent($node, $content);
+                $nodeContent = $dom->innerXML($node);
+                $content = call_user_func_array(array($customTag, 'render'), array($nodeContent, $node));
+                $dom->replaceNodeWithContent($node, $content);
             }
         }
 
-        return $this->dom->saveHTML();
+        return $dom->saveHTML();
+
+
     }
 
 
-    public function initializeComponentParsing() {
+    public function initializeComponentParsing()
+    {
 
 
-    	$template=$this;
+        $template = $this;
 
-        $this->registerCustomTag($this->defaultComponentTagName, function($nodeContent, \DOMElement $node) use ($template) {
+        $this->registerCustomTag($this->defaultComponentTagName, function ($nodeContent, \DOMElement $node) use ($template) {
 
-            $className=(string) $node->getAttribute($this->componantClassNameAttributeName);
+            $className = (string)$node->getAttribute($this->componantClassNameAttributeName);
 
-            if(class_exists($className)) {
+            if (class_exists($className)) {
 
 
-	            $component=new $className;
-	            $component->loadFromDOMNode($node);
-	            $component->bindAttributesValues($this->getVariables());
+                $component = new $className;
+                $component->loadFromDOMNode($node);
+                $component->bindAttributesValues($this->getVariables());
 
                 return $component;
             }
@@ -141,39 +169,32 @@ class Template
     }
 
 
+    public function render($template = null, $values = null)
+    {
 
-    public function render($template=null, $values=null) {
-
-        if($template) {
-            $this->template=$template;
+        if ($template) {
+            $this->template = $template;
         }
 
 
-        if(count($values)) {
+        if (count($values)) {
             $this->setVariables($values);
         }
 
+        $compiledDom = $this->parseDOM($this->template, true);
+        $output = $this->compileMustache($compiledDom, $this->getVariables());
 
 
-
-
-        $compiledDom=$this->parseDOM($this->template);
-
-	    $output=$this->compileMustache($compiledDom, $this->getVariables());
-
-
-        $this->output=$output;
+        $this->output = $output;
         return $this->output;
     }
 
 
-
-
-    public function getOutput($template=null, $values=null) {
-        if($this->output===null) {
+    public function getOutput($template = null, $values = null)
+    {
+        if ($this->output === null) {
             return $this->render($template, $values);
-        }
-        else {
+        } else {
             return $this->output;
         }
     }
